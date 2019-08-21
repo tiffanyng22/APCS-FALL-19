75fe0facf1375036b850500b1dca7f59cb4d8ea0
APCS3rd <APCS3rd@MHS-LT9-16>
2019-08-21 11:16:05 -0700

    complete Hello World Project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,11 @@
+//Tiffany Ng
+//August 18, 2019
+//Write "Hello World" 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+System.out.println("Hello, world!");
+System.out.println("Hello, world!");
+	}
+
+}
