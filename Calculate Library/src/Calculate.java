3f4003dbe32d5f622d3545331be7d611af067680
tiffany <tiffany@10.145.142.88>
2019-09-03 10:28:38 -0700

    Calculate Part1

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,27 @@
+/*Tiffany Ng
+ * August 29, 2019
+ * Library of math functions
+*/
+
+public class Calculate {
+	// a call to square returns the square of the value passed. The method accepts an integer and square it
+	public static  int square(int number) {
+		int answer=0;
+		answer=number * number;
+		return answer;
+	}
+	//a call to to cube returns the cube of the value passed The method accepts an integer and cube it. 
+	public static  int cube(int number) {
+		int answer=0;
+		answer=number * number * number;
+		return answer;			
+    }
+	// a call to average reutns the average of the values passed to it. The method accepts an integer and find the average of them.
+  /*  public static  int average(int number) {
+    	int answer=0
+    	answer=number * number* 1/number
+    	return answer;
+    }
+    */
+	
+    }
