5f11abc2ffadb4cf4560fd6d1e7ce69bef90d356
tiffany <tiffany@10.145.142.88>
2019-09-03 10:27:56 -0700

    Calculate part1

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/*Tiffany Ng
+ * Period 3
+ * August 29,2019
+ *Calls function from Calculate
+ */
+public class DoMath {
+	
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-3));
+		System.out.println(Calculate.cube(-3));
+		
+	}
+}
